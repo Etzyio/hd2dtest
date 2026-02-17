d0e7069080edf5d13283f7c25fcdea71237ca5ad
etzyio <t209314865@qq.com>
2026-02-17 10:06:45 +0800

    refactor: 移除LoadJsonFile方法中的程序集卸载和缓存清理逻辑

--- Scripts/Utilities/JsonHelper.cs
+++ Scripts/Utilities/JsonHelper.cs
@@ -98,10 +98,6 @@ namespace hd2dtest.Scripts.Utilities
 
         public static T LoadJsonFile<T>(string filePath, JsonSerializerOptions options, out bool success)
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            var weaks = new List<WeakReference<AssemblyLoadContext>>();
-            UnloadAssemblyRecursive(assembly, weaks, true, true);
-            clearCacheMethod?.Invoke(null, new object?[] { null }); 
             success = false;
             try
             {
